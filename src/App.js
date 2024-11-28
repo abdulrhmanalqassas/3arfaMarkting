a9023a2ecc761e1c53892e8f57c58393b04fb5eb
abdulrhmanalqassas <abdulrhmanalqassas@gmail.com>
2024-11-28 03:16:03 +0200

    add sub need to be hash

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import './App.css';
-import './assets/css/tailwind.css'
-import './assets/css/materialdesignicons.min.css'
-import { Route, Routes } from 'react-router-dom';
-import Index from './pages';
-import IndexTwo from './pages/index-two';
-import IndexThree from './pages/index-three';
-import IndexFour from './pages/index-four';
-import IndexFive from './pages/index-five';
-import IndexSix from './pages/index-six';
-import IndexSeven from './pages/index-seven';
+import "./App.css";
+import "./assets/css/tailwind.css";
+import "./assets/css/materialdesignicons.min.css";
+import { Route, Routes } from "react-router-dom";
+import Index from "./pages";
+import IndexTwo from "./pages/index-two";
+import IndexThree from "./pages/index-three";
+import IndexFour from "./pages/index-four";
+import IndexFive from "./pages/index-five";
+import IndexSix from "./pages/index-six";
+import IndexSeven from "./pages/index-seven";
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<IndexTwo/>}/>
+      <Route path="/3arfaMarkting/" element={<IndexTwo />} />
       {/* <Route path='/index-two' element={<IndexTwo/>}/>
       <Route path='/index-three' element={<IndexThree/>}/>
       <Route path='/index-four' element={<IndexFour/>}/>
